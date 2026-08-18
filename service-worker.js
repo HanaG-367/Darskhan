83ebbbb325c6a5e60ebc641eb1563ea65cecd092
Hana <forgivingmerciful367@gmail.com>
2026-08-19 00:55:26 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = "dars-khan-v1";
+
+
+const FILES = [
+
+    "./",
+
+    "./index.html",
+
+    "./style.css",
+
+    "./app.js",
+
+    "./manifest.json"
+
+];
+
+
+
+self.addEventListener(
+    "install",
+    function(event){
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+            .then(function(cache){
+
+                return cache.addAll(FILES);
+
+            })
+
+        );
+
+    }
+);
+
+
+
+self.addEventListener(
+    "fetch",
+    function(event){
+
+        event.respondWith(
+
+            caches.match(event.request)
+            .then(function(response){
+
+                return response ||
+                fetch(event.request);
+
+            })
+
+        );
+
+    }
+);
